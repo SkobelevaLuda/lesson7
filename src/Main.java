d5c9ecf12cff147d907e1ebb1d3b33419da8fded
Людок <skobeleva.ludmila@yandex.ru>
2022-12-21 15:51:04 +0300

    Задание 2.2 и 2.3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        example();
+    }
+        private static void example(){
+            List<List<String>> biDemArrList = new ArrayList<>();
+            for (int i = 0; i < 8; i++) {
+                biDemArrList.add(i, new ArrayList<>());
+                for (int j = 0; j < 8; j++) {
+                    biDemArrList.get(i).add(j, ((i + j) % 2 == 1 ? "●" : "◯"));
+                }
+            }
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    System.out.print(biDemArrList.get(i).get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        /*
         Queue<String> kass1= new ArrayDeque<>();
          Queue<String> kass2= new ArrayDeque<>();
         Supermarket.randomFill(kass1);
@@ -16,9 +35,5 @@ public class Main {
 
         Supermarket.remove(kass1,kass2);
         System.out.println(" Первая очередь: "+kass1);
-        System.out.println(" Вторая очередь: "+kass2);
-
-
-
+        System.out.println(" Вторая очередь: "+kass2);*/
     }
-}
